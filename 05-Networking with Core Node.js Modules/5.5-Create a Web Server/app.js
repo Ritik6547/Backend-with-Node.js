a1d588e05a9a32ea8429f05df9a892bba5f3951d
Ritik <alphaop6547@gmail.com>
2025-11-01 01:26:46 +0530

    build a web server using http module

---
+++ 05-Networking with Core Node.js Modules/5.5-Create a Web Server/app.js
@@ -0,0 +1,21 @@
+import { createReadStream } from "node:fs";
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  if (req.url === "/") {
+    const readStream = createReadStream("./public/index.html");
+    readStream.pipe(res);
+  } else {
+    const readStream = createReadStream(`./public${req.url}`);
+    readStream.on("error", (err) => {
+      console.log(err.message);
+      res.end("Not Found");
+    });
+    readStream.pipe(res);
+  }
+});
+
+server.listen(4000, "0.0.0.0", () => {
+  console.log("Server started");
+});
