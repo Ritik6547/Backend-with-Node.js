bc9edf416e06a2dca935061a596c31fe65cc6093
Ritik <alphaop6547@gmail.com>
2026-01-02 23:23:26 +0530

    master virtuals in mongoose

---
+++ 08-MVC Architecture with Mongoose/8.12-Virtuals in Mongoose/UserModel.js
@@ -0,0 +1,88 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name field is required"],
+      minLength: [3, "name should be minimum of length 3"],
+      trim: true,
+      alias: "nam",
+    },
+    age: {
+      type: Number,
+      required: [true, "age field is required"],
+      min: 12,
+      max: 100,
+      validate: {
+        validator: function () {
+          console.log("Running Custom Validator");
+          return this.age % 2 === 0;
+        },
+        message: "age can only be an even number",
+      },
+    },
+    email: {
+      type: String,
+      required: [true, "email field is required"],
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email",
+      ],
+      lowercase: true,
+      trim: true,
+    },
+    hobbies: [String],
+    parentId: {
+      type: Schema.Types.ObjectId,
+      required: function () {
+        return this.age < 16;
+      },
+      default: null,
+      ref: "User",
+    },
+  },
+  {
+    strict: "throw",
+    timestamps: true,
+    versionKey: "__v", // false
+    collection: "users",
+    virtuals: {
+      isAdult: {
+        get() {
+          return this.age >= 18;
+        },
+      },
+      hobbiesString: {
+        get() {
+          let str = "";
+          this.hobbies.forEach((hobby, i) => {
+            str += hobby;
+            if (i != this.hobbies.length - 1) {
+              str += ", ";
+            }
+          });
+          return str;
+        },
+        set(value) {
+          const newHobbies = value.split(", ");
+          this.hobbies = [...this.hobbies, ...newHobbies];
+        },
+      },
+    },
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
+
+UserSchema.virtual("emailDomain").get(function () {
+  return this.email.split("@")[1];
+});
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
---
+++ 08-MVC Architecture with Mongoose/8.12-Virtuals in Mongoose/app.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import "./db.js";
+import User from "./UserModel.js";
+
+const user = await User.findOne({ email: "ritik@gmail.com" });
+
+console.log(user.isAdult);
+console.log(user.hobbiesString);
+console.log(user.emailDomain);
+
+user.hobbiesString = "TT, Football";
+console.log(user.hobbiesString);
+
+await user.save();
+
+console.log(user.schema.virtuals);
+// console.log(user.toJSON({ virtuals: true }));
+
+// console.log(user.toObject());
+
+await mongoose.disconnect();
+console.log("Database Disconnected");
