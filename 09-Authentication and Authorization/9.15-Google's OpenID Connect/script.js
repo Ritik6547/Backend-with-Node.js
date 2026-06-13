b526366ef592085e25cf6558eca323c0b245a9f3
Ritik <alphaop6547@gmail.com>
2026-06-13 23:16:48 +0530

    open popup for google login

--- 09-Authentication and Authorization/9.15-Google's OpenID Connect/script.js
+++ 09-Authentication and Authorization/9.15-Google's OpenID Connect/script.js
@@ -1,13 +1,34 @@
-const code = new URLSearchParams(location.search).get("code");
+const button = document.querySelector("button");
 
 const client_id =
   "183518767383-qors1qe29inuoslplrsj7sr3sepajim3.apps.googleusercontent.com";
-
-const client_secret = "client secret";
-
+const client_secret = "Client Secret";
 const URI = "http://localhost:5500";
+const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${client_id}&scope=openid%20email%20profile&redirect_uri=${URI}`;
 
-const fetchIdToken = async () => {
+button.addEventListener("click", (e) => {
+  // Open Popup
+  window.open(authUrl, "auth-popup", "left=150,top=180,width=420,height=420");
+});
+
+window.addEventListener("message", ({ data }) => {
+  console.log(data);
+
+  if (data.code) {
+    fetchIdToken(data.code);
+  }
+});
+
+if (window.name === "auth-popup") {
+  const code = new URLSearchParams(location.search).get("code");
+
+  if (code) {
+    window.opener.postMessage({ code });
+    window.close();
+  }
+}
+
+const fetchIdToken = async (code) => {
   const payload = `code=${code}&client_id=${client_id}&client_secret=${client_secret}&redirect_uri=${URI}&grant_type=authorization_code`;
 
   const res = await fetch("https://oauth2.googleapis.com/token", {
@@ -30,5 +51,3 @@ const fetchIdToken = async () => {
 
   console.log(userInfo);
 };
-
-if (code) fetchIdToken();
